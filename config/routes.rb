b5b67c5b442305b2dca00edeadef93180ebd7306
Allan James Lapid <ajLapid718@gmail.com>
2017-09-08 04:55:21 -0400

    Change root based on if user_signed_in?

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  root 'welcome#index'
-
   resources :notes
+
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root" # In Rails 5, the addition of the alias is unnecessary
+  end
+
+  root 'welcome#index'
 end
